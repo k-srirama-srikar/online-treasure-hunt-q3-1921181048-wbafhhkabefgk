e545f726aabec52824499e52460821171dfa5f83
K Srirama Srikar <143777264+k-srirama-srikar@users.noreply.github.com>
2024-08-24 20:41:23 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,36 +1,3 @@
-# from flask import Flask, render_template, request, redirect, url_for
-
-# app = Flask(__name__)
-
-
-# @app.route("/")
-# def hello_world():
-#     return render_template("page.html")
-
-
-# @app.route("/check", methods=["GET", "POST"])
-# def check():
-#     c2 = False
-#     ans2 = ""
-
-#     if request.method == "POST":
-#         pwd = request.form['pwd']
-#         if pwd == "aurjaoobt":
-#             c1 = True
-#             ans1 = "first link"
-#         else:
-#             return render_template("page.html")
-#     return render_template("page.html", c1=c2, ans2=ans2)
-
-
-
-# if __name__ == '__main__':
-#     app.run()
-
-
-
-
-
 from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 import json
 import os
@@ -38,20 +5,6 @@ import os
 
 app = Flask(__name__)
 app.secret_key = "e7193bbd8a4ab1dc8bf29c8de5024helloworldjava10269d9c3cc59bf475f1486e811c6fd5996a0a0873ddb63b07b"
-
-# DATA_FILE = 'user_data.json'
-
-# # Function to load data from the JSON file
-# def load_data():
-#     if os.path.exists(DATA_FILE):
-#         with open(DATA_FILE, 'r') as file:
-#             return json.load(file)
-#     return {}
-
-# # Function to save data to the JSON file
-# def save_data(data):
-#     with open(DATA_FILE, 'w') as file:
-#         json.dump(data, file, indent=4)
 
 users = ["weasley@1223414432", "dumbledore@1761837661", "lovegood@1846657233", "granger@280282620", "riddle@-931195728", "malfoy@1081489288"]
 
@@ -72,19 +25,6 @@ def check():
     user_id = request.form['user_id']
     pwd = request.form['pwd']
     
-    # Load existing data
-    # data = load_data()
-    
-    # # Store the new input by user ID
-    # if user_id in data:
-    #     data[user_id].append(pwd)
-    # else:
-    #     data[user_id] = [pwd]
-    
-    # # Save data to file
-    # save_data(data)
-    
-    # return jsonify({"message": "Data saved successfully!", "data": data})
 
     if request.method == "POST":
         if user_id in users:
